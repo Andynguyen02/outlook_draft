3e8567d038ddcb417858b8058b95a205761aa8aa
Andynguyen02 <39106060+Andynguyen02@users.noreply.github.com>
2023-09-01 22:47:49 -0700

    Refactored outlook_draft.py

--- src/outlook_draft.py
+++ src/outlook_draft.py
@@ -1,11 +1,114 @@
+# Import standard libraries
+import datetime
+import logging
+from threading import Lock
+
+# Import async libraries
+import asyncio
+import aiohttp
+
+# Import third-party libraries
+import openai
+from msal import ConfidentialClientApplication
+from transformers import GPT4Model, GPT4Tokenizer
+
+# Initialize logging
+logging.basicConfig(filename='outlook_draft.log', level=logging.INFO)
+
+# Initialize a lock for thread-safety
+lock = Lock()
+
+def initialize_msal_client():
+    """Initialize the MSAL confidential client."""
+    client_id = "<Your-App-Client-ID>"
+    client_secret = "<Your-App-Client-Secret>"
+    authority = "https://login.microsoftonline.com/<Your-Tenant-ID>"
+    return ConfidentialClientApplication(client_id, client_secret=client_secret, authority=authority)
+
+def initialize_openai():
+    """Initialize the OpenAI GPT-4 API."""
+    openai.api_key = "<Your-OpenAI-API-Key>"
+
+def initialize_gpt4():
+    """Initialize GPT-4 model and tokenizer."""
+    tokenizer = GPT4Tokenizer.from_pretrained('gpt-4')
+    model = GPT4Model.from_pretrained('gpt-4')
+    return tokenizer, model
+
+app = initialize_msal_client()
+scopes = ["https://graph.microsoft.com/.default"]
+initialize_openai()
+tokenizer, model = initialize_gpt4()
+
+async def get_unread_emails(session):
+    """Fetch unread emails from Outlook."""
+    async with session.get('https://outlook.office.com/api/v2.0/me/mailfolders/inbox/messages?$filter=IsRead eq false') as resp:
+        return await resp.json()
+
+async def generate_draft_reply(session, email):
+    """
+    Generate a draft reply for an email using GPT-4.
+    
+    Args:
+        session (aiohttp.ClientSession): The aiohttp session.
+        email (dict): The email data.
+        
+    Returns:
+        str: The generated reply.
+    """
+    try:
+        inputs = tokenizer.encode(email['Body']['Content'], return_tensors='pt')
+        outputs = model.generate(inputs, max_length=500, num_return_sequences=1)
+        reply = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return reply
+    except Exception as e:
+        logging.error(f"Failed to generate draft reply due to {str(e)}")
+
+async def save_draft_reply(session, draft_content, email):
+    """
+    Save the draft reply in Outlook.
+    
+    Args:
+        session (aiohttp.ClientSession): The aiohttp session.
+        draft_content (str): The content of the draft.
+        email (dict): The email data.
+    """
+    draft = {
+        'Subject': f"RE: {email['Subject']}",
+        'Body': {
+            'ContentType': 'Text',
+            'Content': draft_content
+        },
+        'ToRecipients': [
+            {
+                'EmailAddress': {
+                    'Address': email['From']['EmailAddress']['Address']
+                }
+            }
+        ]
+    }
+    await session.post('https://outlook.office.com/api/v2.0/me/messages', json=draft)
+
+async def process_email(session, email):
+    """
+    Process an email: generate a draft reply and save it.
+    
+    Args:
+        session (aiohttp.ClientSession): The aiohttp session.
+        email (dict): The email data.
+    """
+    try:
+        draft_content = await generate_draft_reply(session, email)
+        await save_draft_reply(session, draft_content, email)
+    except Exception as e:
+        logging.error(f"Failed to process email due to {str(e)}")
+
 async def main():
+    """Main function to process unread emails."""
     async with aiohttp.ClientSession() as session:
         emails = await get_unread_emails(session)
         if emails is not None:
-            tasks = []
-            for email in emails:
-                task = process_email(session, email)
-                tasks.append(task)
+            tasks = [process_email(session, email) for email in emails]
             await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
