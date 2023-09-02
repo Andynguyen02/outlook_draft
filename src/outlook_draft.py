198ee8ce64ee6f658a8e8937b1515accc77118b6
Andynguyen02 <39106060+Andynguyen02@users.noreply.github.com>
2023-09-01 23:18:31 -0700

    Update outlook_draft.py

--- src/outlook_draft.py
+++ src/outlook_draft.py
@@ -1,16 +1,18 @@
-# Import standard libraries
-import datetime
+# Standard library imports
+import asyncio
 import logging
+import os
 from threading import Lock
 
-# Import async libraries
-import asyncio
+# Third-party imports
 import aiohttp
-
-# Import third-party libraries
 import openai
+from dotenv import load_dotenv
 from msal import ConfidentialClientApplication
 from transformers import GPT4Model, GPT4Tokenizer
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Initialize logging
 logging.basicConfig(filename='outlook_draft.log', level=logging.INFO)
@@ -18,98 +20,128 @@ logging.basicConfig(filename='outlook_draft.log', level=logging.INFO)
 # Initialize a lock for thread-safety
 lock = Lock()
 
-def initialize_msal_client():
-    """Initialize the MSAL confidential client."""
-    client_id = "<Your-App-Client-ID>"
-    client_secret = "<Your-App-Client-Secret>"
-    authority = "https://login.microsoftonline.com/<Your-Tenant-ID>"
-    return ConfidentialClientApplication(client_id, client_secret=client_secret, authority=authority)
+class EmailProcessor:
+    def __init__(self, msal_client, openai_api, gpt4_model, gpt4_tokenizer):
+        """
+        Initializes an EmailProcessor object.
 
-def initialize_openai():
-    """Initialize the OpenAI GPT-4 API."""
-    openai.api_key = "<Your-OpenAI-API-Key>"
+        Args:
+            msal_client (ConfidentialClientApplication): The MSAL client for authentication.
+            openai_api (str): The OpenAI API key.
+            gpt4_model (GPT4Model): The GPT-4 model for generating draft replies.
+            gpt4_tokenizer (GPT4Tokenizer): The GPT-4 tokenizer for encoding and decoding text.
+        """
+        self.msal_client = msal_client
+        self.openai_api = openai_api
+        self.gpt4_model = gpt4_model
+        self.gpt4_tokenizer = gpt4_tokenizer
 
-def initialize_gpt4():
-    """Initialize GPT-4 model and tokenizer."""
-    tokenizer = GPT4Tokenizer.from_pretrained('gpt-4')
-    model = GPT4Model.from_pretrained('gpt-4')
-    return tokenizer, model
+    async def get_unread_emails(self, session):
+        """
+        Retrieves unread emails from the Outlook API.
 
-app = initialize_msal_client()
-scopes = ["https://graph.microsoft.com/.default"]
-initialize_openai()
-tokenizer, model = initialize_gpt4()
+        Args:
+            session (aiohttp.ClientSession): The aiohttp client session.
 
-async def get_unread_emails(session):
-    """Fetch unread emails from Outlook."""
-    async with session.get('https://outlook.office.com/api/v2.0/me/mailfolders/inbox/messages?$filter=IsRead eq false') as resp:
-        return await resp.json()
+        Returns:
+            dict: The JSON response containing the unread emails.
+        """
+        try:
+            async with session.get('https://outlook.office.com/api/v2.0/me/mailfolders/inbox/messages?$filter=IsRead eq false') as resp:
+                return await resp.json()
+        except Exception as e:
+            logging.error(f"Failed to fetch unread emails due to {str(e)}")
+            return None
 
-async def generate_draft_reply(session, email):
-    """
-    Generate a draft reply for an email using GPT-4.
-    
-    Args:
-        session (aiohttp.ClientSession): The aiohttp session.
-        email (dict): The email data.
-        
-    Returns:
-        str: The generated reply.
-    """
-    try:
-        inputs = tokenizer.encode(email['Body']['Content'], return_tensors='pt')
-        outputs = model.generate(inputs, max_length=500, num_return_sequences=1)
-        reply = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        return reply
-    except Exception as e:
-        logging.error(f"Failed to generate draft reply due to {str(e)}")
+    async def generate_draft_reply(self, session, email):
+        """
+        Generates a draft reply for an email using the GPT-4 model.
 
-async def save_draft_reply(session, draft_content, email):
-    """
-    Save the draft reply in Outlook.
-    
-    Args:
-        session (aiohttp.ClientSession): The aiohttp session.
-        draft_content (str): The content of the draft.
-        email (dict): The email data.
-    """
-    draft = {
-        'Subject': f"RE: {email['Subject']}",
-        'Body': {
-            'ContentType': 'Text',
-            'Content': draft_content
-        },
-        'ToRecipients': [
-            {
-                'EmailAddress': {
-                    'Address': email['From']['EmailAddress']['Address']
-                }
+        Args:
+            session (aiohttp.ClientSession): The aiohttp client session.
+            email (dict): The email to generate a draft reply for.
+
+        Returns:
+            str: The generated draft reply.
+        """
+        try:
+            inputs = self.gpt4_tokenizer.encode(email['Body']['Content'], return_tensors='pt')
+            outputs = self.gpt4_model.generate(inputs, max_length=500, num_return_sequences=1)
+            reply = self.gpt4_tokenizer.decode(outputs[0], skip_special_tokens=True)
+            return reply
+        except Exception as e:
+            logging.error(f"Failed to generate draft reply due to {str(e)}")
+            return None
+
+    async def save_draft_reply(self, session, draft_content, email):
+        """
+        Saves a draft reply for an email in the Outlook API.
+
+        Args:
+            session (aiohttp.ClientSession): The aiohttp client session.
+            draft_content (str): The content of the draft reply.
+            email (dict): The email to reply to.
+        """
+        try:
+            draft = {
+                'Subject': f"RE: {email['Subject']}",
+                'Body': {
+                    'ContentType': 'Text',
+                    'Content': draft_content
+                },
+                'ToRecipients': [
+                    {
+                        'EmailAddress': {
+                            'Address': email['From']['EmailAddress']['Address']
+                        }
+                    }
+                ]
             }
-        ]
-    }
-    await session.post('https://outlook.office.com/api/v2.0/me/messages', json=draft)
+            await session.post('https://outlook.office.com/api/v2.0/me/messages', json=draft)
+        except Exception as e:
+            logging.error(f"Failed to save draft reply due to {str(e)}")
 
-async def process_email(session, email):
-    """
-    Process an email: generate a draft reply and save it.
-    
-    Args:
-        session (aiohttp.ClientSession): The aiohttp session.
-        email (dict): The email data.
-    """
-    try:
-        draft_content = await generate_draft_reply(session, email)
-        await save_draft_reply(session, draft_content, email)
-    except Exception as e:
-        logging.error(f"Failed to process email due to {str(e)}")
+    async def process_email(self, session, email):
+        """
+        Processes an email by generating a draft reply and saving it as a draft.
 
-async def main():
-    """Main function to process unread emails."""
-    async with aiohttp.ClientSession() as session:
-        emails = await get_unread_emails(session)
-        if emails is not None:
-            tasks = [process_email(session, email) for email in emails]
-            await asyncio.gather(*tasks)
+        Args:
+            session (aiohttp.ClientSession): The aiohttp client session.
+            email (dict): The email to process.
+        """
+        try:
+            draft_content = await self.generate_draft_reply(session, email)
+            if draft_content is not None:
+                await self.save_draft_reply(session, draft_content, email)
+        except Exception as e:
+            logging.error(f"Failed to process email due to {str(e)}")
+
+    async def main(self):
+        """
+        Main method for processing unread emails.
+
+        This method retrieves unread emails, processes each email, and saves draft replies.
+        """
+        try:
+            async with aiohttp.ClientSession() as session:
+                emails = await self.get_unread_emails(session)
+                if emails is not None:
+                    tasks = [self.process_email(session, email) for email in emails]
+                    await asyncio.gather(*tasks)
+        except Exception as e:
+            logging.error(f"Failed to process emails due to {str(e)}")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    client_id = os.getenv('CLIENT_ID')  # Use os.getenv
+    client_secret = os.getenv('CLIENT_SECRET')  # Use os.getenv
+    authority = os.getenv('AUTHORITY')  # Use os.getenv
+    msal_client = ConfidentialClientApplication(client_id, client_secret=client_secret, authority=authority)
+
+    openai_api_key = os.getenv('OPENAI_API_KEY')  # Use os.getenv
+    openai.api_key = openai_api_key
+
+    gpt4_model = GPT4Model.from_pretrained('gpt-4')
+    gpt4_tokenizer = GPT4Tokenizer.from_pretrained('gpt-4')
+
+    processor = EmailProcessor(msal_client, openai_api_key, gpt4_model, gpt4_tokenizer)
+    asyncio.run(processor.main())
